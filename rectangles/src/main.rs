9c81c5a6e38aa5cd975f5f52762a26b5dde7764e
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-08-11 09:29:03 +0900

    finish 5.3

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,6 +4,23 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
@@ -12,11 +29,22 @@ fn main() {
     println!("rect1 is {:#?}", rect1);
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
-    println!("{}", rect1.height);
-}
 
-fn area(dimensions: &Rectangle) -> u32 {
-    dimensions.width * dimensions.height
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    // rect1にrect2ははまり込む？
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(25);
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&sq));
 }
