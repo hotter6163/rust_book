49033e29174d621d6514bad3c55e6253f01d9cb8
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-08-13 07:52:08 +0900

    finish 6.2

--- chapter6/src/main.rs
+++ chapter6/src/main.rs
@@ -1,14 +1,43 @@
-enum IpAddr {
-    V4(u8, u8, u8, u8),
-    V6(String),
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
 }
 
 fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
-    let loopback = IpAddr::V6(String::from("::1"));
+    println!("{}", value_in_cents(Coin::Penny));
+    println!("{}", value_in_cents(Coin::Quarter(UsState::Alabama)));
 
-    let some_number = Some(5);
-    let some_string = Some("a string");
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+}
 
-    let absent_number: Option<i32> = None;
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
 }
