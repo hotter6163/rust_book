96daf8957a925402d37a2a4c8b899b1371766135
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-08-17 08:00:07 +0900

    finish 6.3

--- chapter6/src/main.rs
+++ chapter6/src/main.rs
@@ -18,6 +18,16 @@ fn main() {
     let five = Some(5);
     let six = plus_one(five);
     let none = plus_one(None);
+
+    let some_u8_value = Some(3u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    };
+
+    if let Some(3) = some_u8_value {
+        println!("three")
+    }
 }
 
 fn value_in_cents(coin: Coin) -> u32 {
