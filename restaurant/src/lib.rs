5233169f6d9ed1d8f6325c7491e74d5890fe1aaf
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-08-19 09:03:07 +0900

    finish 7.5

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,21 +1,9 @@
-use rand::Rng;
-use std::{
-    collections::HashMap,
-    fmt::Result,
-    io::{self, Write},
-};
+mod front_of_house;
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-    }
-}
-
-pub use self::front_of_house::hosting;
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
     hosting::add_to_waitlist();
     hosting::add_to_waitlist();
-    let secret_number = rand::thread_rng().gen_range(1..101);
 }
