7316566cda536885e1ed229a3803d008499ba1d0
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-09-04 08:44:31 +0900

    finish 9.2

--- chapter9/src/main.rs
+++ chapter9/src/main.rs
@@ -1,4 +1,24 @@
+mod read_username_from_file;
+
+use read_username_from_file::read_username_from_file;
+use std::{fs::File, io::ErrorKind};
+
 fn main() {
-    let v = vec![1, 2, 3];
-    v[99];
+    // let f = File::open("hello.txt").expect("Failed to open hello.txt");
+
+    let s = read_username_from_file().unwrap();
+    println!("{}", s)
+
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(ref error) if error.kind() == ErrorKind::NotFound => match File::create("hello.txt") {
+    //         Ok(fc) => fc,
+    //         Err(e) => {
+    //             panic!("Tried to create file but there was a problem: {:?}", e)
+    //         }
+    //     },
+    //     Err(error) => {
+    //         panic!("There was a problem opening the file: {:?}", error)
+    //     }
+    // };
 }
